a1269acffc4f4ab6100018f78dd8632717826fae
manishparteykwl <manish.partey@kwglobal.com>
2023-08-22 13:59:46 +0530

    Committing DBContext

--- MRF.DataAccess/Data/MRFDBContext.cs
+++ MRF.DataAccess/Data/MRFDBContext.cs
@@ -9,7 +9,32 @@ namespace MRF.DataAccess.Data
         {
 
         }
-
         public DbSet<Candidatestatusmaster> Candidatestatusmaster { get; set; }
+        public DbSet<Departmentmaster> Departmentmaster { get; set; }
+        public DbSet<Employmenttypemaster> Employmenttypemaster { get; set; }
+        public DbSet<Evaluationfeedbackmaster> Evaluationfeedbackmaster { get; set; }
+        public DbSet<Evaluationmaster> Evaluationmaster { get; set; }
+        public DbSet<Evaluationstatusmaster> Evaluationstatusmaster { get; set; }
+        public DbSet<Gendermaster> Gendermaster { get; set; }
+        public DbSet<Grademaster> Grademaster { get; set; }
+        public DbSet<Locationmaster> Locationmaster { get; set; }
+        public DbSet<Mrfstatusmaster> Mrfstatusmaster { get; set; }
+        public DbSet<Projectmaster> Projectmaster { get; set; }
+        public DbSet<Qualificationmaster> Qualificationmaster { get; set; }
+        public DbSet<Rolemaster> Rolemaster { get; set; }
+        public DbSet<Subdepartmentmaster> Subdepartmentmaster { get; set; }
+        public DbSet<Vacancytypemaster> Vacancytypemaster { get; set; }
+        public DbSet<Candidatedetail> Candidatedetail { get; set; }
+        public DbSet<Employeedetail> Employeedetail { get; set; }
+        public DbSet<Employeelogindetail> Employeelogindetail { get; set; }
+        public DbSet<Employeerolemap> Employeerolemap { get; set; }
+        public DbSet<Freshmrfdetail> Freshmrfdetail { get; set; }
+        public DbSet<Interviewevaluation> Interviewevaluation { get; set; }        
+        public DbSet<Mrfdetail> Mrfdetail { get; set; }
+        public DbSet<Mrffeedback> Mrffeedback { get; set; }
+        public DbSet<Mrfinterviewermap> Mrfinterviewermap { get; set; }
+        public DbSet<Mrfresumereviewermap> Mrfresumereviewermap { get; set; }
+        public DbSet<Replacementmrfdetail> Replacementmrfdetail { get; set; }
+        public DbSet<Resumeforwarddetail> Resumeforwarddetail { get; set; }
     }
 }
