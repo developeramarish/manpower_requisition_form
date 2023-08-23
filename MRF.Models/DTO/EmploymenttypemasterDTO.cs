58e432ff10e40aabf643c0c2870a5fe2ad888806
manishparteykwl <manish.partey@kwglobal.com>
2023-08-23 19:29:12 +0530

    Committing changes for EmploymenttypeController

--- MRF.Models/DTO/EmploymenttypemasterDTO.cs
+++ MRF.Models/DTO/EmploymenttypemasterDTO.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace MRF.Models.DTO
 {
-    internal class EmploymenttypemasterDTO
+    public class EmploymenttypemasterRequestModel
     {
+        public string Type { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public int CreatedByEmployeeId { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public int UpdatedByEmployeeId { get; set; }
+
+        public DateTime UpdatedOnUtc { get; set; }
+    }
+    public class EmploymenttypemasterResponseModel
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
     }
 }
---
+++ MRF.Service/Controllers/EmploymenttypeController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRF.DataAccess.Repository;
+using MRF.DataAccess.Repository.IRepository;
+using MRF.Models.DTO;
+using MRF.Models.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MRF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmploymenttypeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private ResponseDTO _response;
+        private EmploymenttypemasterResponseModel _responseModel;
+        private readonly ILogger<DepartmentController> _logger;
+        public EmploymenttypeController(IUnitOfWork unitOfWork, ILogger<DepartmentController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ResponseDTO();
+            _responseModel = new EmploymenttypemasterResponseModel();
+            _logger = logger;
+        }
+        // GET: api/<EmploymenttypeController>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(IEnumerable<Employmenttypemaster>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                List<Employmenttypemaster> obj = _unitOfWork.Employmenttypemaster.GetAll().ToList();
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // GET api/<EmploymenttypeController>/5
+        [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(Employmenttypemaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Employmenttypemaster employmenttypemaster = _unitOfWork.Employmenttypemaster.Get(u => u.Id == id);
+                if (employmenttypemaster == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "No result found by this Id: " + id;
+                    _logger.LogError("No result found by this Id:" + id);
+                }
+                _response.Result = employmenttypemaster;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // POST api/<EmploymenttypeController>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, Description = "Item created successfully", Type = typeof(Employmenttypemaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public EmploymenttypemasterResponseModel Post([FromBody] EmploymenttypemasterRequestModel request)
+        {
+            try
+            {
+                var employmenttype = new Employmenttypemaster
+                {                  
+                    IsActive = request.IsActive,
+                    CreatedByEmployeeId = request.CreatedByEmployeeId,
+                    CreatedOnUtc = request.CreatedOnUtc,
+                    UpdatedByEmployeeId = request.UpdatedByEmployeeId,
+                    UpdatedOnUtc = request.UpdatedOnUtc
+                };
+
+                _unitOfWork.Employmenttypemaster.Add(employmenttype);
+                _unitOfWork.Save();
+
+                _responseModel.Id = employmenttype.Id;
+                _responseModel.IsActive = employmenttype.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+
+            return _responseModel;
+        }
+
+        // PUT api/<EmploymenttypeController>/5
+        [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item updated successfully", Type = typeof(Employmenttypemaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful update)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public EmploymenttypemasterResponseModel Put(int id, [FromBody] EmploymenttypemasterRequestModel request)
+        {
+            try
+            {
+                var existingStatus = _unitOfWork.Employmenttypemaster.Get(u => u.Id == id);                
+                existingStatus.IsActive = request.IsActive;
+                existingStatus.UpdatedByEmployeeId = request.UpdatedByEmployeeId;
+                existingStatus.UpdatedOnUtc = request.UpdatedOnUtc;
+
+                _unitOfWork.Employmenttypemaster.Update(existingStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = existingStatus.Id;
+                _responseModel.IsActive = existingStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _responseModel;
+        }
+
+        // DELETE api/<EmploymenttypeController>/5
+        [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item deleted successfully", Type = typeof(Employmenttypemaster))]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful deletion)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public void Delete(int id)
+        {
+            try
+            {
+                Employmenttypemaster? obj = _unitOfWork.Employmenttypemaster.Get(u => u.Id == id);
+                _unitOfWork.Employmenttypemaster.Remove(obj);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+        }
+    }
+}
