e6a1aa8c18eed46e27922139b6f024c468a9e61b
manishparteykwl <manish.partey@kwglobal.com>
2023-08-24 16:27:44 +0530

    Committing RoleController

--- MRF.Models/DTO/RolemasterDTO.cs
+++ MRF.Models/DTO/RolemasterDTO.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MRF.Models.DTO
+﻿namespace MRF.Models.DTO
 {
-    internal class RolemasterDTO
+
+    public class RolemasterRequestModel
+    {  
+        public string Name { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public int CreatedByEmployeeId { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public int UpdatedByEmployeeId { get; set; }
+        public DateTime UpdatedOnUtc { get; set; }
+    }
+    public class RolemasterResponseModel
     {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
     }
 }
---
+++ MRF.Service/Controllers/RoleController.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRF.DataAccess.Repository.IRepository;
+using MRF.Models.DTO;
+using MRF.Models.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MRF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private ResponseDTO _response;
+        private RolemasterResponseModel _responseModel;
+        private readonly ILogger<RoleController> _logger;
+        public RoleController(IUnitOfWork unitOfWork, ILogger<RoleController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ResponseDTO();
+            _responseModel = new RolemasterResponseModel();
+            _logger = logger;
+        }
+        // GET: api/<RoleController>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(IEnumerable<Rolemaster>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                List<Rolemaster> obj = _unitOfWork.Rolemaster.GetAll().ToList();
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // GET api/<RoleController>/5
+        [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(Rolemaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Rolemaster  rolemaster = _unitOfWork.Rolemaster.Get(u => u.Id == id);
+                if (rolemaster == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "No result found by this id: " + id;
+                    _logger.LogError("No result found by this id:" + id);
+                }
+                _response.Result = rolemaster;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // POST api/<RoleController>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, Description = "Item created successfully", Type = typeof(RolemasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public RolemasterResponseModel Post([FromBody] RolemasterRequestModel request)
+        {
+            try
+            {
+                var roleStatus = new Rolemaster
+                {
+                    Name = request.Name,
+                    IsActive = request.IsActive,
+                    CreatedByEmployeeId = request.CreatedByEmployeeId,
+                    CreatedOnUtc = request.CreatedOnUtc,
+                    UpdatedByEmployeeId = request.UpdatedByEmployeeId,
+                    UpdatedOnUtc = request.UpdatedOnUtc
+                };
+
+                _unitOfWork.Rolemaster.Add(roleStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = roleStatus.Id;
+                _responseModel.IsActive = roleStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+
+            return _responseModel;
+        }
+
+        // PUT api/<RoleController>/5
+        [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item updated successfully", Type = typeof(RolemasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful update)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public RolemasterResponseModel Put(int id, [FromBody] RolemasterRequestModel request)
+        {
+            try
+            {
+                var existingStatus = _unitOfWork.Rolemaster.Get(u => u.Id == id);
+                existingStatus.Name = request.Name;
+                existingStatus.IsActive = request.IsActive;
+                existingStatus.UpdatedByEmployeeId = request.UpdatedByEmployeeId;
+                existingStatus.UpdatedOnUtc = request.UpdatedOnUtc;
+
+                _unitOfWork.Rolemaster.Update(existingStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = existingStatus.Id;
+                _responseModel.IsActive = existingStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _responseModel;
+        }
+
+        // DELETE api/<RoleController>/5
+        [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item deleted successfully", Type = typeof(RolemasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful deletion)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public void Delete(int id)
+        {
+            try
+            {
+                Rolemaster? obj = _unitOfWork.Rolemaster.Get(u => u.Id == id);
+                _unitOfWork.Rolemaster.Remove(obj);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+        }
+    }
+}
