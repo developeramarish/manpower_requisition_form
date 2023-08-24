966d2d22894710fe362a3513f716b5c3214a2988
manishparteykwl <manish.partey@kwglobal.com>
2023-08-24 11:30:59 +0530

    Committing GenderController

---
+++ MRF.Service/Controllers/GenderController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRF.DataAccess.Repository.IRepository;
+using MRF.Models.DTO;
+using MRF.Models.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MRF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenderController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private ResponseDTO _response;
+        private GendermasterResponseModel _responseModel;
+        private readonly ILogger<GenderController> _logger;
+        public GenderController(IUnitOfWork unitOfWork, ILogger<GenderController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ResponseDTO();
+            _responseModel = new GendermasterResponseModel();
+            _logger = logger;
+        }
+        // GET: api/<GenderController>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(IEnumerable<Gendermaster>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                List<Gendermaster> obj = _unitOfWork.Gendermaster.GetAll().ToList();
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // GET api/<GenderController>/5
+        [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(Gendermaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Gendermaster gendermaster  = _unitOfWork.Gendermaster.Get(u => u.Id == id);
+                if (gendermaster == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "No result found by this Id: " + id;
+                    _logger.LogError("No result found by this Id:" + id);
+                }
+                _response.Result = gendermaster;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // POST api/<GenderController>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, Description = "Item created successfully", Type = typeof(GendermasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public GendermasterResponseModel PostPost([FromBody] GendermasterRequestModel request)
+        {
+            try
+            {
+                var genderStatus = new Gendermaster
+                {   
+                    IsActive = request.IsActive,
+                    CreatedByEmployeeId = request.CreatedByEmployeeId,
+                    CreatedOnUtc = request.CreatedOnUtc,
+                    UpdatedByEmployeeId = request.UpdatedByEmployeeId,
+                    UpdatedOnUtc = request.UpdatedOnUtc
+                };
+
+                _unitOfWork.Gendermaster.Add(genderStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = genderStatus.Id;                
+                _responseModel.IsActive = genderStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+
+            return _responseModel;
+        }
+
+        // PUT api/<GenderController>/5
+        [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item updated successfully", Type = typeof(GendermasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful update)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public GendermasterResponseModel Put(int id, [FromBody] GendermasterRequestModel request)
+        {
+            try
+            {
+                var existingStatus = _unitOfWork.Gendermaster.Get(u => u.Id == id);
+                existingStatus.IsActive = request.IsActive;
+                existingStatus.UpdatedByEmployeeId = request.UpdatedByEmployeeId;
+                existingStatus.UpdatedOnUtc = request.UpdatedOnUtc;
+
+                _unitOfWork.Gendermaster.Update(existingStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = existingStatus.Id;
+                _responseModel.IsActive = existingStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _responseModel;
+        }
+
+        // DELETE api/<GenderController>/5
+        [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item deleted successfully", Type = typeof(GendermasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful deletion)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public void Delete(int id)
+        {
+            try
+            {
+                Gendermaster? obj = _unitOfWork.Gendermaster.Get(u => u.Id == id);
+                _unitOfWork.Gendermaster.Remove(obj);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+        }
+    }
+}
