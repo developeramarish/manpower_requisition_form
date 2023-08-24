39767ac26e839472eb3a4e357eef5336a553a4e8
manishparteykwl <manish.partey@kwglobal.com>
2023-08-24 10:46:35 +0530

    Committing EvaluationController

--- MRF.Models/DTO/EvaluationmasterDTO.cs
+++ MRF.Models/DTO/EvaluationmasterDTO.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace MRF.Models.DTO
 {
-    internal class EvaluationmasterDTO
+    public class EvaluationmasterRequestModel
     {
+        public string Type { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public int CreatedByEmployeeId { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        public int UpdatedByEmployeeId { get; set; }
+
+        public DateTime UpdatedOnUtc { get; set; }
+
+    }
+    public class EvaluationmasterResponseModel
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
     }
 }
---
+++ MRF.Service/Controllers/EvaluationController.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRF.DataAccess.Repository.IRepository;
+using MRF.Models.DTO;
+using MRF.Models.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MRF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvaluationController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private ResponseDTO _response;
+        private EvaluationmasterResponseModel _responseModel;
+        private readonly ILogger<EvaluationController> _logger;
+        public EvaluationController(IUnitOfWork unitOfWork, ILogger<EvaluationController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ResponseDTO();
+            _responseModel = new EvaluationmasterResponseModel();
+            _logger = logger;
+        }
+        // GET: api/<EvaluationController>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(IEnumerable<Evaluationmaster>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                List<Evaluationmaster> obj = _unitOfWork.Evaluationmaster.GetAll().ToList();
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // GET api/<EvaluationController>/5
+        [HttpGet("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Successful response", Type = typeof(Evaluationmaster))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Evaluationmaster evaluationmaster = _unitOfWork.Evaluationmaster.Get(u => u.Id == id);
+                if (evaluationmaster == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "No result found by this Id: " + id;
+                    _logger.LogError("No result found by this Id:" + id);
+                }
+                _response.Result = evaluationmaster;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _response;
+        }
+
+        // POST api/<EvaluationController>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status201Created, Description = "Item created successfully", Type = typeof(EvaluationmasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad Request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal Server Error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public EvaluationmasterResponseModel Post([FromBody] EvaluationmasterRequestModel request)
+        {
+            try
+            {
+                var evaluationStatus = new Evaluationmaster
+                {
+                    
+                    IsActive = request.IsActive,
+                    CreatedByEmployeeId = request.CreatedByEmployeeId,
+                    CreatedOnUtc = request.CreatedOnUtc,
+                    UpdatedByEmployeeId = request.UpdatedByEmployeeId,
+                    UpdatedOnUtc = request.UpdatedOnUtc
+                };
+
+                _unitOfWork.Evaluationmaster.Add(evaluationStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = evaluationStatus.Id;                
+                _responseModel.IsActive = evaluationStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+
+            return _responseModel;
+        }
+
+        // PUT api/<EvaluationController>/5
+        [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item updated successfully", Type = typeof(EvaluationmasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful update)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity, Description = "Unprocessable entity")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public EvaluationmasterResponseModel Put(int id, [FromBody] EvaluationmasterRequestModel request)
+        {
+            try
+            {
+                var existingStatus = _unitOfWork.Evaluationmaster.Get(u => u.Id == id);
+                
+                existingStatus.IsActive = request.IsActive;
+                existingStatus.UpdatedByEmployeeId = request.UpdatedByEmployeeId;
+                existingStatus.UpdatedOnUtc = request.UpdatedOnUtc;
+
+                _unitOfWork.Evaluationmaster.Update(existingStatus);
+                _unitOfWork.Save();
+
+                _responseModel.Id = existingStatus.Id;
+                _responseModel.IsActive = existingStatus.IsActive;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+            return _responseModel;
+        }
+
+        // DELETE api/<EvaluationController>/5
+        [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Item deleted successfully", Type = typeof(EvaluationmasterResponseModel))]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "No content (successful deletion)")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Bad request")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Unauthorized")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "Forbidden")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Not Found")]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+        [SwaggerResponse(StatusCodes.Status503ServiceUnavailable, Description = "Service Unavailable")]
+        public void Delete(int id)
+        {
+            try
+            {
+                Evaluationmaster? obj = _unitOfWork.Evaluationmaster.Get(u => u.Id == id);
+                _unitOfWork.Evaluationmaster.Remove(obj);
+                _unitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                _logger.LogError(ex.Message);
+            }
+        }
+    }
+}
