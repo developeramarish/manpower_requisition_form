01bbae0d0ce2b91c3dd69a3be3b34f9ed395bc96
manishparteykwl <manish.partey@kwglobal.com>
2023-09-05 14:02:10 +0530

    Adding Gateway project

--- MRF/MRF.Gateway/Program.cs
+++ MRF/MRF.Gateway/Program.cs
@@ -1,6 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Config/ocelot." + environment + ".json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
+
