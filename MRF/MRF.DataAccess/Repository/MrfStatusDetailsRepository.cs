224d8cf149682d1975fce788c55684d26bf1cd7e
amitakwl <amita.dhamapurkar@kwglobal.com>
2023-09-18 10:16:34 +0530

    Update MrfStatusDetailsRepository.cs

--- MRF/MRF.DataAccess/Repository/MrfStatusDetailsRepository.cs
+++ MRF/MRF.DataAccess/Repository/MrfStatusDetailsRepository.cs
@@ -1,18 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MRF.DataAccess.Repository.IRepository;
-using MRF.Models.Models;
+﻿using MRF.DataAccess.Repository.IRepository;
 using MRF.Models.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Data;
-
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MRF.DataAccess.Repository
 {
+    
     public class MrfStatusDetailsRepository : Repository<MrfDetailsViewModel>, IMrfStatusDetailsRepository
     {
         private readonly Data.MRFDBContext _db;
@@ -20,38 +13,33 @@ namespace MRF.DataAccess.Repository
         {
             _db = db;
         }
-
+  
         public MrfDetailsViewModel GetMrfStatusDetails(int mrfId)
         {
-            var query = from mrfDetails in _db.Mrfdetails
+            IQueryable<MrfDetailsViewModel> query  = from mrfDetails in _db.Mrfdetails
                         join mrfStatus in _db.Mrfstatusmaster on mrfDetails.MrfStatusId equals mrfStatus.Id
                         join Emp in _db.Employeedetails on mrfDetails.CreatedByEmployeeId equals Emp.Id
                         join salary in _db.Freshmrfdetails on mrfDetails.Id equals salary.Id
                         join Vacancy in _db.Vacancytypemaster on mrfDetails.VacancyTypeId equals Vacancy.Id
-                        where mrfDetails.Id==mrfId
-                        //     group new { mrfDetails, mrfStatus } by new
-                        //     {
-                        //         mrfDetails.Id,
-                        //         mrfDetails.MrfStatusId,
-                        //         mrfDetails.ReferenceNo,
-                        //     }
-                        //into grouped
+                        where mrfDetails.Id == mrfId
                         select new MrfDetailsViewModel
                         {
                             ReferenceNo = mrfDetails.ReferenceNo,
                             Experience = mrfDetails.MinExperience + "-" + mrfDetails.MaxExperience,
                             MrfStatusId = mrfDetails.MrfStatusId,
-                            MrfStatus= mrfStatus.Status,
+                            MrfStatus = mrfStatus.Status,
                             CreatedByEmployeeId = mrfDetails.CreatedByEmployeeId,
-                            Name=Emp.Name,
+                            Name = Emp.Name,
                             CreatedOnUtc = mrfDetails.CreatedOnUtc,
                             UpdatedOnUtc = mrfDetails.UpdatedOnUtc,
                             Salary = salary.MinTargetSalary + "-" + salary.MaxTargetSalary,
-                            VacancyNo=mrfDetails.VacancyNo,
+                            VacancyNo = mrfDetails.VacancyNo,
                             RequisitionType = Vacancy.Type,
                         };
 
             return query.FirstOrDefault();
         }
     }
+
 }
+
