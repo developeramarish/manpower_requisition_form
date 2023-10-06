75b8726f62f9b0a14fb39bb045042851013362d3
GauRav-080 <gauravsingh.rana@kwglobal.com>
2023-10-06 15:58:29 +0530

    added create requisition page

--- Frontend/src/Dashboard.js
+++ Frontend/src/Dashboard.js
@@ -1,18 +1,17 @@
 // Dashboard.js
-import React from 'react';
-import DashboardHeader from './Pages/Header';
-import LeftPanel from './Pages/LeftPanel';
-import DashboardContent from './Pages/DashboardContent';
-
+import React from "react";
+import DashboardHeader from "./Pages/Header";
+import LeftPanel from "./Pages/LeftPanel";
+import DashboardContent from "./Pages/DashboardContent";
 
 const Dashboard = () => {
   return (
     <div>
       <DashboardHeader />
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         <LeftPanel />
         <div className="MyDashboard">
-        <DashboardContent />
+          <DashboardContent />
         </div>
       </div>
     </div>
