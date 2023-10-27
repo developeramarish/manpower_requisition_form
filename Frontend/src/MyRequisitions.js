61cafe1d54f5db3ff383757111113b5278a4efe5
KritikaGuptaKGL <144150431+KritikaGuptaKGL@users.noreply.github.com>
2023-10-27 13:35:51 +0530

    Added My Requisition Page
    Added My Requisition Page

--- Frontend/src/MyRequisitions.js
+++ Frontend/src/MyRequisitions.js
@@ -1,86 +1,65 @@
-import React, { useState } from 'react';
+import React, {useEffect,useState } from 'react';
+import axios from "axios"; 
 import './MyRequisitions.css';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { DataViewLayoutOptions } from 'primereact/dataview';
+import DataTableComponents from './Components/DataTableComponent';
+ 
+
 import DashboardHeader from './Pages/Header';
 import LeftPanel from './Pages/LeftPanel';
 import SearchText from './Pages/SearchText';
- 
- 
+
+
+
  
     const MyRequisitions = () => {
-        // Dummy data
-        const [data, setData] = useState([
-          { MRFID: '01/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '01-05-2023',RequisionType:
-        'New',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Sent'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '01-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-03-2023',RequisionType:
-        'ReNew',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/005', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'ReNew',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '05/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-04-2023',RequisionType:
-        'New',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'ReNew',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '06/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'ReNew',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'ReNew',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'New',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'New',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'},
-        { MRFID:  '02/MUM/CFR/JAN/15/003', CreatedBy: '03-05-2023',CreatedOn:'03-05-2023',LastUpdated: '03-05-2023',RequisionType:
-        'New',NoOfPositions:2,ExpRequired:2,SalaryRange:'2-4 LPA',Status:'Draft'}
-          // Add more dummy data as needed
-        ]);
-
-
-   
-    
-    
-    return(
- 
-        
-        <div >
-        <DashboardHeader />
-        <div style={{ display: 'flex' }}>
-          <LeftPanel />
-         
-          <div className = "bar">
-            <div class="containerH">
-                <label class="box" >My Requisitions</label>
-                <div class="SearchText"><SearchText/></div>
-            </div>
-          
-        
       
-      
+      const [data, setData] = useState([{}]);
        
+      React.useEffect(() => {
+        const url = "https://localhost:7128/api/Mrfdetail/GetMrfDetails/0";
+        fetch(url)
+          .then((response) => {
+            return response.json()
+          })
+          .then((json) => {
+            setData(json['result'])
+          })
           
+          .catch((error) => console.log(error));
+      }, []);
+      console.log(data)
+         const columns = [
+        {columnName : 'MRF ID', field : 'referenceNo'},
+        {columnName : 'Created By', field : 'name'},
+        {columnName : 'CreatedOn', field : 'createdOnUtc'},
+        {columnName : 'LastUpdated', field : 'updatedOnUtc'},
+        {columnName : 'RequisionType', field : 'requisitionType'},
+        {columnName : 'NoOfPositions', field : 'vacancyNo'},
+        {columnName : 'ExpRequired', field : 'experience'},
+        {columnName : 'SalaryRange', field : 'salary'},
+        {columnName : 'Status', field : 'mrfStatus'}
         
-        
-      
+      ]
+   
+    return (
+      <div >
+      <DashboardHeader />
+      <div style={{ display: 'flex' }}>
+        <LeftPanel />
+       
         <div className = "bar">
- 
-            
-            <DataTable value={data} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '69rem'}} >
-    <Column field="MRFID" header="MRF ID" style={{ width: '20%',color:'#0e0808'}} ></Column>
-    <Column field="CreatedBy" header="Created By"sortable style={{ width: '25%' ,color:'#0e0808'}}></Column>
-    <Column field="CreatedOn" header="Created On"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="LastUpdated" header="Last Updated"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="RequisionType" header="Requision Type"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="NoOfPositions" header="No.of positions"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="ExpRequired" header="Exp Required"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="SalaryRange" header="Salary Range"sortable style={{ width: '25%',color:'#0e0808'}}></Column>
-    <Column field="Status" header="Status"sortable style={{ width: '25  %',color:'#0e0808'}}></Column>
-    </DataTable>
+          <div class="containerH">
+              <label class="box" >My Requisitions</label>
+              <div class="SearchText"><SearchText/></div>
+          </div>
+          
+           
+    <div className = "bar"><DataTableComponents data= {data} columns={columns} rows={5}/></div>
+    </div> 
     </div>
-      </div>
-      </div>
-    </div>
-     
-        
+  </div>
     );
+    
+     
     }
     export default MyRequisitions ;
