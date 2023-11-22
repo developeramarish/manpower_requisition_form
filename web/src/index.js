d522fa0e356e82d9e743f7d535c56723465b0b10
alpesh_patil200787 <64024817+alpeshpatil200787@users.noreply.github.com>
2023-11-22 12:35:39 +0530

    Revert "web folder added"
    This reverts commit bdc1a83593c370d3d818e28e873170fc4240a8a4.

--- web/src/index.js
+++ web/src/index.js
@@ -4,32 +4,10 @@ import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
-
-import { Provider } from 'react-redux';
-import store from "./store/store";
-
-/**
- * Initialize a PublicClientApplication instance which is provided to the MsalProvider component
- * We recommend initializing this outside of your root component to ensure it is not re-initialized on re-renders
- */
-const msalInstance = new PublicClientApplication(msalConfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-/**
- * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
- */
 root.render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-
-    </MsalProvider>
+    <App />
   </React.StrictMode>
 );
 
