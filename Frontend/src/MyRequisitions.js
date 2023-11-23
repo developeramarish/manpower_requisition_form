3334fcf867b157786cc0b37dcce45d409f022b70
GauRav-080 <gauravsingh.rana@kwglobal.com>
2023-11-23 13:40:42 +0530

    added css of myrequisiton
    added css of myrequisiton and changed create requisition css

--- Frontend/src/MyRequisitions.js
+++ Frontend/src/MyRequisitions.js
@@ -18,54 +18,5 @@ const MyRequisitions = () => {
       </div>
     </>
   );
-
-  // const [data, setData] = useState([{}]);
-  // //if we pass id 0 then ge get all the data otherwise we get specific data like id=1 means
-  // React.useEffect(() => {
-  //   const url = "https://localhost:7128/api/Mrfdetail/GetMrfDetails/0";
-  //   fetch(url)
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((json) => {
-  //       setData(json["result"]);
-  //     })
-
-  //     .catch((error) => console.log(error));
-  // }, []);
-  // console.log(data);
-  // const columns = [
-  //   { columnName: "MRF ID", field: "referenceNo" },
-  //   { columnName: "Created By", field: "name" },
-  //   { columnName: "Created On", field: "createdOnUtc" },
-  //   { columnName: "Last Updated", field: "updatedOnUtc" },
-  //   { columnName: "Requision Type", field: "requisitionType" },
-  //   { columnName: "No Of Positions", field: "vacancyNo" },
-  //   { columnName: "Exp Required", field: "experience" },
-  //   { columnName: "Salary Range", field: "salary" },
-  //   { columnName: "Status", field: "mrfStatus" },
-  // ];
-
-  // return (
-  //   <div>
-  //     <DashboardHeader />
-  //     <div style={{ display: "flex" }}>
-  //       <LeftPanel />
-
-  //       <div className="bar">
-  //         <div class="containerH">
-  //           <label class="box">My Requisitions</label>
-  //           <div class="SearchText">
-  //             <SearchText />
-  //           </div>
-  //         </div>
-
-  //         <div className="bar">
-  //           <DataTableComponents data={data} columns={columns} rows={5} />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 export default MyRequisitions;
