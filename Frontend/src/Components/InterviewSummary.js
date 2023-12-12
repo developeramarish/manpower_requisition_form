0e1d1ca690dd727afcf6be185937cc82b2e72a7a
GauRav-080 <gauravsingh.rana@kwglobal.com>
2023-12-12 11:59:44 +0530

    added changes to interview summary

--- Frontend/src/Components/InterviewSummary.js
+++ Frontend/src/Components/InterviewSummary.js
@@ -3,253 +3,251 @@ import { Dialog } from "primereact/dialog";
 import { DataTable } from "primereact/datatable";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
-import { APIPath } from "../Components/constant";
-import { constantResumePath, constantAssignmentPath } from "./constant";
+import { Dropdown } from "primereact/dropdown";
+import { MultiSelect } from "primereact/multiselect";
+import {
+	APIPath,
+	Roles,
+	constantAssignmentPath,
+	constantResumePath,
+} from "../Components/constant";
 import "../styles/layout/InterviewSummary.css";
-import MultiSelectDropdown from "./multiselectDropdown";
 import InterviewFeedbackComponent from "./InterviewFeedbackComponent";
 
+const roleId = 3; //need to get role id from local storage
+
+const strToArray = (s) => {
+	if (typeof s === "string") {
+		s = s.split(",").map(Number);
+	}
+	return s;
+};
+
+//for multiselect because it doesn't work properly
+const arrayToObj = (options = [], selectedOpt) => {
+	if (Array.isArray(selectedOpt)) {
+		return options.filter((e) => selectedOpt.includes(e.employeeId));
+	}
+	return [selectedOpt];
+};
+
+const objToArray = (selectedOpt = []) => {
+	return selectedOpt.map((e) => e.employeeId);
+};
+
+const uploadedOnBodyTemplate = (interview) => {
+	return new Date(interview.createdOnUtc)
+		.toISOString()
+		.slice(0, 10)
+		.replaceAll("/", "-");
+};
+
+const resumeBodyTemplate = (interview) => {
+	let resumeLink = constantResumePath + interview.resumePath;
+	return (
+		<a href={resumeLink} target="_blank" className="int-link-cell">
+			{interview.resumePath}
+		</a>
+	);
+};
+
+const attachmentBodyTemplate = (interview) => {
+	let attachmentLink = constantAssignmentPath + interview.attachment;
+	return (
+		<a href={attachmentLink} target="_blank" className="int-link-cell">
+			Assignment
+		</a>
+	);
+};
+
+//summary component
 const InterviewSummary = ({ visible, onHide, mrfId = 1 }) => {
-  const [interviewData, setInterviewData] = useState([]);
-  const [interviewStatus, setInterviewStatus] = useState([]);
-  const [interviewerData, setInterviewerData] = useState([]);
-  const [saveBttn, setSaveBttn] = useState([]);
-  const [showFeed, setShowFeed] = useState(false);
-  const [feedRow, setFeedRow] = useState({});
+	const [interviewData, setInterviewData] = useState([]);
+	const [interviewStatus, setInterviewStatus] = useState([]);
+	const [interviewerData, setInterviewerData] = useState([]);
+	const [saveBttn, setSaveBttn] = useState([]);
+	const [showFeed, setShowFeed] = useState(false);
+	const [selectedId, setSelectedId] = useState(null);
 
-  useEffect(() => {
-    const apiUrl =
-      APIPath +
-      `Mrfinterviewermap/GetInterviewDetails/GetInterviewDetails?id=${mrfId}&DashBoard=true`;
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((response) => {
-        const data = response.result;
-        let arr = new Array(data.interviewDetails.length).fill(false); // for save bttn
-        setInterviewData(data.interviewDetails);
-        setInterviewStatus(
-          data.interviewstatus.filter((x) => x.candidateorEvalution == "E")
-        );
-        setInterviewerData(data.interviewReviewer);
-        setSaveBttn(arr);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  }, []);
+	useEffect(() => {
+		const apiUrl =
+			APIPath +
+			`Mrfinterviewermap/GetInterviewDetails/GetInterviewDetails?id=${mrfId}&DashBoard=true`;
+		fetch(apiUrl)
+			.then((response) => response.json())
+			.then((response) => {
+				const data = response.result;
+				let arr = new Array(data.interviewDetails.length).fill(false); // for save bttn
+				setInterviewData(data.interviewDetails);
+				setInterviewStatus(
+					data.interviewstatus.filter((x) => x.candidateorEvalution == "E")
+				);
+				setInterviewerData(data.interviewReviewer);
+				setSaveBttn(arr);
+			})
+			.catch((error) => {
+				console.error("Fetch error:", error);
+			});
+	}, [mrfId]);
 
-  const openPdfInNewTab = (pdfLink) => {
-    window.open(pdfLink, "_blank");
-  };
+	const statusBodyTemplate = (interview, options) => {
+		if (roleId === Roles.mrfOwner) {
+			let is = interviewStatus.filter((x) => x.id == interview.evaluationId);
+			return <p className="drop-width">{is[0].status}</p>;
+		}
 
-  const strToArray = (s) => {
-    if (typeof s === "string") {
-      s = s.split(",").map(Number);
-    }
-    return s;
-  };
+		return (
+			<Dropdown
+				optionLabel="status"
+				optionValue="id"
+				className="drop-width"
+				options={interviewStatus}
+				value={interview.evaluationId}
+				onChange={(e) => {
+					let interviewDataCopy = [...interviewData];
+					let sv = [...saveBttn];
+					sv[options.rowIndex] = true;
+					interviewDataCopy[options.rowIndex].evaluationId = e.target.value;
+					setInterviewData(interviewDataCopy);
+					setSaveBttn(sv);
+				}}
+			/>
+		);
+	};
 
-  const arrayToObj = (options = [], selectedOpt) => {
-    if (Array.isArray(selectedOpt)) {
-      return options.filter((e) => selectedOpt.includes(e.employeeId));
-    }
-    return [selectedOpt];
-  };
+	const interviewerBodyTemplate = (interview, options) => {
+		return (
+			<MultiSelect
+				className="drop-width"
+				options={interviewerData}
+				value={arrayToObj(interviewerData, interview.interviewerEmployeeId)}
+				onChange={(e) => {
+					//let interviewDataCopy = JSON.parse(JSON.stringify(interviewData));
+					let interviewDataCopy = [...interviewData];
+					let sv = [...saveBttn];
+					sv[options.rowIndex] = e.value.length > 0 ? true : false;
+					interviewDataCopy[options.rowIndex].interviewerEmployeeId =
+						objToArray(e.value);
+					setInterviewData(interviewDataCopy);
+					setSaveBttn(sv);
+				}}
+				optionLabel="name"
+				// optionValue="employeeId"
+			/>
+		);
+	};
 
-  const objToArray = (selectedOpt = []) => {
-    return selectedOpt.map((e) => e.employeeId);
-  };
+	const feedbackBodyTemplate = (interview) => {
+		if (interview.evaluationId < 5) return "To be updated";
+		return (
+			<p
+				onClick={() => {
+					setSelectedId(interview.candidateId);
+					setShowFeed(true);
+				}}
+				className="int-link-cell"
+			>
+				Click Here
+			</p>
+		);
+	};
 
-  const uploadedOnBodyTemplate = (interview) => {
-    return new Date(interview.createdOnUtc)
-      .toLocaleDateString()
-      .replaceAll("/", "-");
-  };
+	const actionBodyTemplate = (interview, options) => {
+		if (saveBttn[options.rowIndex]) {
+			return <Button icon="pi pi-save" />;
+		}
+		return <Button icon="pi pi-save" disabled />;
+	};
 
-  const statusBodyTemplate = (interview, options) => {
-    let is = interviewStatus.filter((x) => x.id == interview.evaluationId);
-    return <p className="drop-width">{is[0].status}</p>;
-    // return (
-    //   <DropdownComponent
-    //     optionLabel="status"
-    //     optionValue="id"
-    //     className="drop-width"
-    //     options={interviewStatus}
-    //     value={interview.evaluationId}
-    //     onChange={(e) => {
-    //       let interviewDataCopy = JSON.parse(JSON.stringify(interviewData));
-    //       let sv = [...saveBttn];
-    //       sv[options.rowIndex] = true;
-    //       interviewDataCopy[options.rowIndex].evaluationId = e.target.value;
-    //       setInterviewData(interviewDataCopy);
-    //       setSaveBttn(sv);
-    //     }}
-    //   />
-    // );
-  };
+	const columns = [
+		{
+			field: "referenceNo",
+			header: "Sr. No.",
+			body: (data, options) => options.rowIndex + 1,
+			bodyClassName: "int-edit-col",
+		},
+		{
+			field: "resumePath",
+			header: "Resume",
+			body: resumeBodyTemplate,
+			sortable: true,
+		},
+		{
+			field: "createdOnUtc",
+			header: "Uploaded On",
+			body: uploadedOnBodyTemplate,
+			sortable: true,
+		},
+		{
+			field: "createdName",
+			header: "Uploaded By",
+			sortable: true,
+		},
+		{
+			field: "interviewerName",
+			header: "Interviewer/ Panel",
+			body: interviewerBodyTemplate,
+			bodyClassName: "drop-col",
+			sortable: true,
+		},
+		{
+			field: "evaluationId",
+			header: "Interview Status",
+			body: statusBodyTemplate,
+			bodyClassName: "drop-col",
+			sortable: true,
+		},
+		{
+			field: "evalutionStatus",
+			header: "Interview Feedback",
+			body: feedbackBodyTemplate,
+		},
+		{
+			field: "attachment",
+			header: "Attachment",
+			body: attachmentBodyTemplate,
+		},
+		{
+			header: "Action",
+			body: actionBodyTemplate,
+			bodyClassName: "int-edit-col",
+		},
+	];
 
-  const resumeBodyTemplate = (interview) => {
-    let resumeLink = constantResumePath + interview.resumePath;
-    return (
-      <a
-        href={resumeLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="int-link-cell"
-        onClick={(e) => {
-          e.preventDefault();
-          openPdfInNewTab(resumeLink);
-        }}
-      >
-        {interview.resumePath}
-      </a>
-    );
-  };
-
-  const interviewerBodyTemplate = (interview, options) => {
-    return (
-      <MultiSelectDropdown
-        className="drop-width"
-        options={interviewerData}
-        value={arrayToObj(interviewerData, interview.interviewerEmployeeId)}
-        onChange={(e) => {
-          let interviewDataCopy = JSON.parse(JSON.stringify(interviewData));
-          let sv = [...saveBttn];
-          sv[options.rowIndex] = e.value.length > 0 ? true : false;
-          interviewDataCopy[options.rowIndex].interviewerEmployeeId =
-            objToArray(e.value);
-          setInterviewData(interviewDataCopy);
-          setSaveBttn(sv);
-          console.log(interviewData);
-        }}
-        optionLabel="name"
-        // optionValue="employeeId"
-      />
-    );
-  };
-  const feedbackBodyTemplate = (interview) => {
-    return (
-      <p
-        onClick={() => {
-          setFeedRow(interview);
-          setShowFeed(true);
-        }}
-        className="int-link-cell"
-      >
-        Click here
-      </p>
-    );
-  };
-
-  const attachmentBodyTemplate = (interview) => {
-    let attachmentLink = constantAssignmentPath + interview.attachment;
-    return (
-      <a
-        href={attachmentLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="int-link-cell"
-        onClick={(e) => {
-          e.preventDefault();
-          openPdfInNewTab(attachmentLink);
-        }}
-      >
-        Assignment
-      </a>
-    );
-  };
-
-  const actionBodyTemplate = (interview, options) => {
-    if (saveBttn[options.rowIndex]) {
-      return <Button icon="pi pi-save" />;
-    }
-    return <Button icon="pi pi-save" disabled />;
-  };
-
-  return (
-    <Dialog
-      header="MRF ID (Interview Summary)"
-      visible={visible}
-      onHide={onHide}
-      draggable={false}
-      className="int-card"
-    >
-      <DataTable
-        value={interviewData}
-        paginator
-        removableSort
-        rows={10}
-        scrollable
-        scrollHeight="62vh"
-        draggable={false}
-      >
-        <Column
-          field="referenceNo"
-          header="Sr. No."
-          body={(data, options) => options.rowIndex + 1}
-          bodyClassName="int-col int-edit-col"
-        />
-        <Column
-          field="resumePath"
-          header="Resume"
-          body={resumeBodyTemplate}
-          bodyClassName="int-col"
-          sortable
-        />
-        <Column
-          field="createdOnUtc"
-          header="Uploaded On"
-          body={uploadedOnBodyTemplate}
-          bodyClassName="int-col"
-          sortable
-        />
-        <Column
-          field="createdName"
-          header="Uploaded By"
-          bodyClassName="int-col"
-          sortable
-        />
-        <Column
-          field="interviewerName"
-          header="Interviewer/ Panel"
-          body={interviewerBodyTemplate}
-          bodyClassName="int-col drop-col"
-          sortable
-        />
-        <Column
-          field="evaluationId"
-          header="Interview Status"
-          body={statusBodyTemplate}
-          bodyClassName="int-col int-status-col"
-          sortable
-        />
-        <Column
-          field="evalutionStatus"
-          header="Interview Feedback"
-          body={feedbackBodyTemplate}
-          bodyClassName="int-col"
-          sortable
-        />
-        <Column
-          field="attachment"
-          header="Attachment"
-          body={attachmentBodyTemplate}
-          bodyClassName="int-col"
-          sortable
-        />
-        <Column
-          header="Action"
-          body={actionBodyTemplate}
-          bodyClassName="int-col int-edit-col"
-        />
-      </DataTable>
-      <InterviewFeedbackComponent
-        visible={showFeed}
-        onHide={() => setShowFeed(false)}
-        intData={feedRow}
-      />
-    </Dialog>
-  );
+	return (
+		<Dialog
+			header="MRF ID (Interview Summary)"
+			visible={visible}
+			onHide={onHide}
+			draggable={false}
+			className="int-card"
+		>
+			<DataTable
+				value={interviewData}
+				paginator
+				removableSort
+				rows={10}
+				scrollable
+				scrollHeight="62vh"
+				draggable={false}
+			>
+				{columns.map((col) => (
+					<Column
+						field={col.field}
+						header={col.header}
+						body={col.body}
+						bodyClassName={"int-col " + col.bodyClassName}
+						sortable={col.sortable}
+					/>
+				))}
+			</DataTable>
+			<InterviewFeedbackComponent
+				visible={showFeed}
+				onHide={setShowFeed}
+				selectedId={selectedId}
+			/>
+		</Dialog>
+	);
 };
 
 export default InterviewSummary;
--- Frontend/src/Components/constant.js
+++ Frontend/src/Components/constant.js
@@ -4,51 +4,61 @@ export const constantResumePath = "https://10.22.11.101:90/Resume/";
 export const constantAssignmentPath = "https://10.22.11.101:90/Assignment/";
 
 export const multiSoftwareSkill = [
-  { name: "Visual Studio", code: "Visual Studio" },
-  { name: "MS Teams", code: "MS Teams" },
-  { name: "MS Office", code: "MS Office" },
-  // Add more options as needed
+	{ name: "Visual Studio", code: "Visual Studio" },
+	{ name: "MS Teams", code: "MS Teams" },
+	{ name: "MS Office", code: "MS Office" },
+	// Add more options as needed
 ];
 export const multiHardwareSkill = [
-  { name: "Laptop", code: "Laptop" },
-  { name: "Headset", code: "Headset" },
-  { name: "Keyboard", code: "Keyboard" },
-  { name: "Mouse", code: "Mouse" },
-  // Add more options as needed
+	{ name: "Laptop", code: "Laptop" },
+	{ name: "Headset", code: "Headset" },
+	{ name: "Keyboard", code: "Keyboard" },
+	{ name: "Mouse", code: "Mouse" },
+	// Add more options as needed
 ];
 
 export const mrfStatus = {
-  draft: 2,
-  submToHr: 3,
-  resubReq: 4,
-  open: 9,
-  rejected: 11,
-  closed: 13,
-  withdrawn: 12,
-  onHold: 10,
+	draft: 2,
+	submToHr: 3,
+	resubReq: 4,
+	open: 9,
+	rejected: 11,
+	closed: 13,
+	withdrawn: 12,
+	onHold: 10,
+};
+
+export const Roles = {
+	superAdmin: 1,
+	admin: 2,
+	mrfOwner: 3,
+	hr: 4,
+	resumeReviwer: 5,
+	interviewer: 6,
+	hiringManager: 7,
 };
 
 export const Gender = [
-  { label: "Male", id: 1 },
-  { label: "Female", id: 2 },
-  { label: "Other", id: 3 },
+	{ label: "Male", id: 1 },
+	{ label: "Female", id: 2 },
+	{ label: "Other", id: 3 },
 ];
 export const minExperienceOptions = Array.from({ length: 31 }, (_, i) => ({
-  label: i.toString(),
-  value: i,
+	label: i.toString(),
+	value: i,
 }));
 export const maxExperienceOptions = Array.from({ length: 31 }, (_, i) => ({
-  label: i.toString(),
-  value: i,
+	label: i.toString(),
+	value: i,
 }));
 
 export const RequisitionType = [
-  { name: "FR", code: "FR" },
-  { name: "RP", code: "RP" },
+	{ name: "FR", code: "FR" },
+	{ name: "RP", code: "RP" },
 
-  // Add more options as needed
+	// Add more options as needed
 ];
 
 export const removeSpaces = (str) => {
-  return str.replace(/\s/g, ""); // This regular expression replaces all spaces globally
+	return str.replace(/\s/g, ""); // This regular expression replaces all spaces globally
 };
