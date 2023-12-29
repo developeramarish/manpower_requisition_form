4ae1fce56a16a8d0b5d26a2d6594e460c94c47b7
amitakwl <amita.dhamapurkar@kwglobal.com>
2023-12-29 19:01:49 +0530

    Update MrfStatusDetailsRepository.cs

--- MRF/MRF.DataAccess/Repository/MrfStatusDetailsRepository.cs
+++ MRF/MRF.DataAccess/Repository/MrfStatusDetailsRepository.cs
@@ -20,31 +20,38 @@ namespace MRF.DataAccess.Repository
         public List<MrfDetailsViewModel> GetMrfStatusDetails(int statusId,int roleId,int userId)
         {
             string Role = _Utility.GetRole(roleId);
-            IQueryable<MrfDetailsViewModel> query  = from mrfDetails in _db.Mrfdetails
-                        join mrfStatus in _db.Mrfstatusmaster on mrfDetails.MrfStatusId equals mrfStatus.Id
-                        join mrfRolemap in _db.mrfStatusrolemap on mrfStatus.Id equals mrfRolemap.statusId
-                        join Emp in _db.Employeedetails on mrfDetails.CreatedByEmployeeId equals Emp.Id
-                        join salary in _db.Freshmrfdetails on mrfDetails.Id equals salary.MrfId
-                        join Vacancy in _db.Vacancytypemaster on mrfDetails.VacancyTypeId equals Vacancy.Id
-                        where mrfRolemap.RoleId== roleId && (statusId == 0 || (statusId !=0 && mrfStatus.Id == statusId)) && (Role != "mrfowner" || (Role == "mrfowner" && mrfDetails.CreatedByEmployeeId == userId))
-                        orderby mrfDetails.UpdatedOnUtc descending
-                        select new MrfDetailsViewModel
-                        {
-                            MrfId = mrfDetails.Id,
-                            ReferenceNo = mrfDetails.ReferenceNo,
-                            Experience = mrfDetails.MinExperience + "-" + mrfDetails.MaxExperience,
-                            MrfStatusId = mrfDetails.MrfStatusId,
-                            //MrfStatus = GetStatus(roleId,mrfStatus.Status),
-                            MrfStatus = mrfStatus.Status,
-                            CreatedByEmployeeId = mrfDetails.CreatedByEmployeeId,
-                            Name = Emp.Name,
-                            CreatedOnUtc = mrfDetails.CreatedOnUtc,
-                            UpdatedOnUtc = mrfDetails.UpdatedOnUtc,
-                            Salary = salary.MinTargetSalary + "-" + salary.MaxTargetSalary,
-                            VacancyNo = mrfDetails.VacancyNo,
-                            RequisitionType = mrfDetails.RequisitionType,
-                            RoleId  = mrfRolemap.RoleId,
-                        };
+            IQueryable<MrfDetailsViewModel> query =
+    from mrfDetails in _db.Mrfdetails
+    join mrfStatus in _db.Mrfstatusmaster on mrfDetails.MrfStatusId equals mrfStatus.Id
+    join mrfRolemap in _db.mrfStatusrolemap on mrfStatus.Id equals mrfRolemap.statusId
+    join Emp in _db.Employeedetails on mrfDetails.CreatedByEmployeeId equals Emp.Id
+    join Vacancy in _db.Vacancytypemaster on mrfDetails.VacancyTypeId equals Vacancy.Id
+    // Left join with Freshmrfdetails
+    join salary in _db.Freshmrfdetails
+         on mrfDetails.Id equals salary.MrfId into freshMrfDetailsGroup
+    from freshMrfDetail in freshMrfDetailsGroup.DefaultIfEmpty()
+    where mrfRolemap.RoleId == roleId &&
+          (statusId == 0 || (statusId != 0 && mrfStatus.Id == statusId)) &&
+          (Role != "mrfowner" || (Role == "mrfowner" && mrfDetails.CreatedByEmployeeId == userId))
+    orderby mrfDetails.UpdatedOnUtc descending
+    select new MrfDetailsViewModel
+    {
+        MrfId = mrfDetails.Id,
+        ReferenceNo = mrfDetails.ReferenceNo,
+        Experience = mrfDetails.MinExperience + "-" + mrfDetails.MaxExperience,
+        MrfStatusId = mrfDetails.MrfStatusId,
+        MrfStatus = mrfStatus.Status,
+        CreatedByEmployeeId = mrfDetails.CreatedByEmployeeId,
+        Name = Emp.Name,
+        CreatedOnUtc = mrfDetails.CreatedOnUtc,
+        UpdatedOnUtc = mrfDetails.UpdatedOnUtc,
+        // Check for null before using null-conditional operator
+        Salary = freshMrfDetail != null ? freshMrfDetail.MinTargetSalary + "-" + freshMrfDetail.MaxTargetSalary : null,
+        VacancyNo = mrfDetails.VacancyNo,
+        RequisitionType = mrfDetails.RequisitionType,
+        RoleId = mrfRolemap.RoleId,
+    };
+
 
             return query.ToList();
         }
