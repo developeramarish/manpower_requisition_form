0b0e60669a088a948992698af7ac12941e470dc9
Ramkrishna-KGL732 <ramkrishna.maurya@kwglobal.com>
2024-01-04 11:09:27 +0530

    common seting

--- web/src/Pages/commonSettings.js
+++ web/src/Pages/commonSettings.js
@@ -100,37 +100,7 @@ export function applySettingsBasedOnRoleAndStatus(
 
       }
 
-    //     switch (mrfStatusId) {
-    //       case MRF_STATUS.submToHr:
-    //         applyCommonSettings({
-
-    //           setReadOnly: false,
-    //           setHodapproval: true,
-    //           setHiringManager: true,
-    //           setSiteHRSPOCApproval: true,
-    //           setFinanceHeadApproval: true,
-    //           setCooapproval: true,
-    //           awatingFinance:true,
-    // recievedFinance:true,
-
-    //       });
-
-    //       case MRF_STATUS.awaitHodApproval:
-    //       case MRF_STATUS.awaitCooApproval:
-    //       case MRF_STATUS.awaitfinanceHeadApproval:
-    //         applyCommonSettings({
-    //           setReadOnly: true,
-    //           setHodapproval: false,
-    //           setHiringManager: false,
-    //           setSiteHRSPOCApproval: false,
-    //           setFinanceHeadApproval: false,
-    //           setCooapproval: true
-    //         });
-    //         break;
-    //       default:
-    //         applyCommonSettings(commonSettings);
-    //     }
-    //     break;
+   
 
     default:
       if (roleId === 3) {
@@ -154,69 +124,3 @@ function applyCommonSettings(settings) {
   Object.assign(commonSettings, settings);
 }
 
-// if (getReqRoleId == 4 && mrfStatusId == MRF_STATUS.submToHr) {
-//   setReadOnly(true);
-//   setHiringManager(false);
-//   setSiteHRSPOCApproval(false)
-//   setHodapproval(false)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(true)
-
-// }
-// else if(getReqRoleId == 4 && mrfStatusId == MRF_STATUS.awaitHodApproval){
-//   setReadOnly(true);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(false)
-//   setFinanceHeadApproval(true)
-// }
-// else if(getReqRoleId == 4 && mrfStatusId == MRF_STATUS.awaitCooApproval){
-//   setReadOnly(true);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(false)
-// }
-// else if(getReqRoleId == 4 && mrfStatusId == MRF_STATUS.awaitfinanceHeadApproval){
-//   setReadOnly(true);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(false)
-// }
-
-// else if (getReqRoleId == 4) {
-//   setReadOnly(true);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(true)
-//   // setEmailApprovalReadOnly(true);
-// } else if (
-//   (getReqRoleId == 3 && mrfStatusId == MRF_STATUS.draft) ||
-//   mrfStatusId == MRF_STATUS.resubReq
-// ) {
-//   setReadOnly(false);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(true)
-// } else if (getReqRoleId == 3) {
-//   setReadOnly(true);
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(true)
-// } else if (roleId == 3) {
-//   setHiringManager(true);
-//   setSiteHRSPOCApproval(true)
-//   setHodapproval(true)
-//   setCooapproval(true)
-//   setFinanceHeadApproval(true)
-// }
