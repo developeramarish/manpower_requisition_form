cbd41a478b5b63a0d59ad174fc3b30c3585e6470
manishparteykwl <Manish.Partey@kwglobal.com>
2024-01-10 20:26:59 +0530

    Done Email Changes

---
+++ MRF/MRF.Models/Models/EmailRecipient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRF.Models.Models
+{
+    public class EmailRecipient
+    {
+        public string Email { get; set; }
+    }
+}
