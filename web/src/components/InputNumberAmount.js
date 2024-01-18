a5396e0fbf23e3cd252e977d59a2eaddf1e373f9
amitakwl <amita.dhamapurkar@kwglobal.com>
2024-01-18 18:48:29 +0530

    added code float

---
+++ web/src/components/InputNumberAmount.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { InputNumber } from "primereact/inputnumber";
+const InputNumberamount = ({id,value,onChange,disable,maxLength,useGrouping}) => {
+  return (
+    <InputNumber
+
+      inputId={id}
+      value={value}
+      onValueChange={onChange}
+      disabled={disable}
+      maxLength={maxLength}
+      className={`bg-gray-100`}
+      useGrouping={useGrouping}
+      minFractionDigits={2} 
+      maxFractionDigits={2}
+      max={100}
+    />
+  );
+};
+
+export default InputNumberamount;
