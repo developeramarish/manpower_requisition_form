289cac74288dbc72127047c96649fe3390872175
manishparteykwl <Manish.Partey@kwglobal.com>
2024-02-09 11:33:16 +0530

    Reverted Config changes

--- web/src/constants/config.js
+++ web/src/constants/config.js
@@ -1,6 +1,6 @@
 export const APP_KEY = "MRF_v1.0";
-//const APP = "https://10.22.11.101:90";
-const APP="https://localhost:7128";
+const APP = "https://10.22.11.101:90";
+//const APP="https://localhost:7128";
 const APIPath=`${APP}/api/`;
 export const API_URL = {
   MRF_STATUS_SUMMARY: `${APIPath}Dashboard/GetMrfStatusSummary`,
