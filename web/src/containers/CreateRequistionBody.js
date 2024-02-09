65e5aa2e21c5341ca68ec836088c196b2519743e
Ramkrishna-KGL732 <ramkrishna.maurya@kwglobal.com>
2024-02-09 16:37:09 +0530

    button name change

--- web/src/containers/CreateRequistionBody.js
+++ web/src/containers/CreateRequistionBody.js
@@ -1968,7 +1968,7 @@ const CreateRequisitionBody = ({
 
           <div className="flex flex-wrap justify-content-end gap-5 mt-3">
             <ButtonC
-              label="CANCEL"
+              label="Close"
               // className=" w-2 surface-hover border-red-600 text-red-600"
               className="cancel_btn"
               onClick={handleCancel}
