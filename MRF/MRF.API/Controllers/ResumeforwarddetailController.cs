ce0a59459b6116288994458ef7d1af642a586079
manishparteykwl <142434295+manishparteykwl@users.noreply.github.com>
2024-02-13 17:00:01 +0530

    Logs updated (#256)

--- MRF/MRF.API/Controllers/ResumeforwarddetailController.cs
+++ MRF/MRF.API/Controllers/ResumeforwarddetailController.cs
@@ -100,7 +100,7 @@ namespace MRF.API.Controllers
                 emailmaster emailRequest = _unitOfWork.emailmaster.Get(u => u.status == "update MRF");
                 if (emailRequest != null)
                 {
-                    _logger.LogInfo(ControllerContext.ActionDescriptor.ControllerName);
+                    _logger.LogInfo("Sending Email from ResumeforwarddetailResponseModel Post");
                     _emailService.SendEmailAsync(emailRequest.emailTo, emailRequest.Subject, emailRequest.Content);
                 }
             }
