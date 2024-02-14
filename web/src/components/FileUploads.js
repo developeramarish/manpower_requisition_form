c6a0ed1908843a496534ecda41221d4366d7b878
Ramkrishna maurya <144430859+Ramkrishna-KGL732@users.noreply.github.com>
2024-02-14 14:32:09 +0530

    Mrf ramkrishna (#268)
    * added no of days and fix: skills
    
    * fix:back button
    
    * fix: resume summary poistion on dashboard
    
    * fix:increase the size of feedback form
    
    * fix: add button in drop down
    
    * fix column in resume summary
    
    * fix: add resume  not redirecting

---
+++ web/src/components/FileUploads.js
@@ -0,0 +1,69 @@
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import ToastMessages from "./ToastMessages";
+import { FileUpload } from "primereact/fileupload";
+
+const FileUploads = forwardRef((props, ref) => {
+  useImperativeHandle(ref, () => ({
+    clearSelectedFile() {
+     
+      if (fileInputRef.current) {
+        fileInputRef.current.clear();
+      }
+      props.onChange(null); // Notify parent component about file reset
+    },
+  }));
+
+  const fileInputRef = useRef(null);
+  const toastRef = useRef(null);
+
+  const onFileSelect = (event) => {
+    const selectedFile =
+      event.files && event.files.length > 0 ? event.files[0] : null;
+
+    if (selectedFile) {
+      const allowedExtension = props.fileExtension
+        .split(",")
+        .map((ext) => ext.trim().toLowerCase());
+      const selectedfileExtension = selectedFile.name
+        .split(".")
+        .pop()
+        .toLowerCase();
+      const fileExtensionIsValid = allowedExtension.includes(
+        selectedfileExtension
+      );
+
+      if (fileExtensionIsValid) {
+        props.onChange(selectedFile);
+      } else {
+        toastRef.current.showWarrningMessage(
+          `Please select a file with following extension ${props.fileExtension}`
+        );
+        fileInputRef.current.clear();
+      }
+    } else {
+      toastRef.current.showWarrningMessage(`Please select a file.`);
+    }
+
+    console.log(selectedFile);
+  };
+
+  return (
+    <div className="fileupload">
+      <FileUpload
+        ref={fileInputRef}
+        mode={"basic"}
+        accept={props.fileExtension}
+        onSelect={onFileSelect}
+        disabled={props.disable}
+      />
+      <ToastMessages ref={toastRef} />
+    </div>
+  );
+});
+
+export default FileUploads;
