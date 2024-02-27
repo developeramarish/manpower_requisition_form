40881069860538e6b8eaf73c7648cd15f291d883
Ramkrishna-KGL732 <ramkrishna.maurya@kwglobal.com>
2024-02-27 11:26:19 +0530

    fix:dropadd new

--- web/src/components/DropDownAddNew.js
+++ web/src/components/DropDownAddNew.js
@@ -14,42 +14,46 @@ const DropdownAddNew = ({
   onAddItem,
 }) => {
   const [filteredOptions, setFilteredOptions] = useState(options);
- 
+
   useEffect(() => {
     setFilteredOptions(options);
   }, [options]);
 
-
   const filterData = (e) => {
-   
     var elem = document.getElementById("dropdown_" + "new"),
       addButton = document.querySelector(".p-dropdown-filter-container");
-    var inputElement = document.querySelector('.p-dropdown-filter-container input');
+    var inputElement = document.querySelector(
+      ".p-dropdown-filter-container input"
+    );
     if (inputElement) {
       var inputValue = inputElement.value;
-    } 
+    }
     if (addButton === e.target) {
       onAddItem(inputValue);
+      addButton.classList.remove("add-enabled");
     }
   };
-
 
   const handleInput = (event) => {
-    const inputElement = document.querySelector('.p-dropdown-filter-container input');
-  if (inputElement) {
-    const inputValue = inputElement.value.toLowerCase();
+    const inputElement = document.querySelector(
+      ".p-dropdown-filter-container input"
+    );
+    if (inputElement) {
+      const inputValue = inputElement.value.toLowerCase();
 
-    const container = document.querySelector('.p-dropdown-filter-container');
-    if (!options.find((option) => option[optionLabel].toLowerCase().includes(inputValue))) {
-     
-      container.classList.add('add-enabled');
-    } else {
-      container.classList.remove('add-enabled');
+      const container = document.querySelector(".p-dropdown-filter-container");
+      if (
+        !options.find((option) =>
+          option[optionLabel].toLowerCase().includes(inputValue)
+        )
+      ) {
+        container.classList.add("add-enabled");
+      } else {
+        container.classList.remove("add-enabled");
+      }
     }
-  }
   };
 
- 
   return (
     <Dropdown
       optionLabel={optionLabel}
@@ -60,9 +64,8 @@ const DropdownAddNew = ({
       placeholder={placeholder}
       className={className}
       disabled={disable}
-      
       onMouseDown={filterData}
-       onInput={handleInput} // Handle input for search
+      onInput={handleInput} // Handle input for search
       filter
     />
   );
