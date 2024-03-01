a56b9671a8a722466177ee3ddb3df74cd3342af1
Ramkrishna-KGL732 <ramkrishna.maurya@kwglobal.com>
2024-03-01 10:53:55 +0530

    uncommented util interview part

--- web/src/constants/Utils.js
+++ web/src/constants/Utils.js
@@ -418,10 +418,10 @@ export const INTERVIEW_EVALUATION_FOR_DISABLE =(roleId,evaluationID)=>{
 }
 
 
-// if((roleId == ROLES.interviewer)&& [INTERVIEW_EVALUATION.CandidateSelected,INTERVIEW_EVALUATION.CandidateNotSelected,INTERVIEW_EVALUATION.FaceToFaceInterviewNotCleared,INTERVIEW_EVALUATION.AssignmentRejected,INTERVIEW_EVALUATION.CodingRoundNotCleared,INTERVIEW_EVALUATION.OfferRejected,INTERVIEW_EVALUATION.Onboarded,INTERVIEW_EVALUATION.OfferacceptedAndDidnotjoin,].includes(evaluationID))
-// {
-//   return  true;
-// }
+if((roleId == ROLES.interviewer)&& [INTERVIEW_EVALUATION.CandidateSelected,INTERVIEW_EVALUATION.CandidateNotSelected,INTERVIEW_EVALUATION.FaceToFaceInterviewNotCleared,INTERVIEW_EVALUATION.AssignmentRejected,INTERVIEW_EVALUATION.CodingRoundNotCleared,INTERVIEW_EVALUATION.OfferRejected,INTERVIEW_EVALUATION.Onboarded,INTERVIEW_EVALUATION.OfferacceptedAndDidnotjoin,].includes(evaluationID))
+{
+  return  true;
+}
 
 
 }
