a3351cd510a567a6fb93d107ba80c81312cf1ed6
KritikaGuptaKGL <144150431+KritikaGuptaKGL@users.noreply.github.com>
2024-03-08 12:47:00 +0530

    solve save as draft and resubbmission

--- MRF/MRF.API/Controllers/MrfinterviewermapController.cs
+++ MRF/MRF.API/Controllers/MrfinterviewermapController.cs
@@ -231,9 +231,9 @@ namespace MRF.API.Controllers
             {
                 _unitOfWork.Mrfinterviewermap.Remove(obj);
                 _unitOfWork.Save();
-                if (_hostEnvironment.IsEnvironment("Development") || _hostEnvironment.IsEnvironment("Production"))
+                /*if (_hostEnvironment.IsEnvironment("Development") || _hostEnvironment.IsEnvironment("Production"))
                 {  
-                    emailmaster emailRequest = _unitOfWork.emailmaster.Get(u => u.status == "Interviewer deleted");
+                    emailmaster emailRequest = _unitOfWork.emailmaster.Get(u => u.status == "Interviewer removed");
 
                     Mrfdetails mrfdetails = GetMrfDetails(obj.MrfId);
                     Employeedetails employeedetails = GetInterviewer(obj.InterviewerEmployeeId);
@@ -252,7 +252,7 @@ namespace MRF.API.Controllers
                     {
                         _emailService.SendEmailAsync(emailReq.Email, emailSubject, emailContent);
                     }
-                }
+                }*/
 
             }
             else
