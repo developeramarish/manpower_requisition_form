f14e81e04a95323acedfc3780690c042b0068bdf
Ramkrishna-KGL732 <ramkrishna.maurya@kwglobal.com>
2024-03-13 19:19:57 +0530

    Update Footer.js

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -4,7 +4,7 @@ import "../css/Footer.css";
 const Footer = () => {
   return (
     <div className="footer 	">
-       Copyright © 2024 KnowledgeWorks Global Ltd. All rights reserved. (v2024R1.1).  
+      Copyright © 2024 KnowledgeWorks Global Ltd. All rights reserved. (v2024R1.2).
     </div>
   );
 };
